87d1fccc1e4eafad0a33e5b7f501a7ba596d175e
Swapnil Raj <swapnilraj85444@gmail.com>
2025-04-24 23:11:44 +0530

    HomePage and Posts Added

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.signup, name='signup'),
     path('login/', views.login, name='login'),
+    path('home/', views.home, name='home'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 def testing(request):
@@ -9,4 +12,16 @@ def login(request):
     return render(request, 'blog/login.html')
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST('uname')
+        email = request.POST('uemail')
+        password = request.POST('upassword')
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+        new_user.save()
     return render(request, 'blog/signup.html')
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
